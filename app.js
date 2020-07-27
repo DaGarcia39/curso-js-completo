07bac0717b26ffd427201cd43f71f11742f01354
DaGarcia39 <dagarcia282@misena.edu.co>
2020-07-26 20:26:13 -0500

    Inicio del curso con los primeros 14 videos hechos

---
+++ app.js
@@ -0,0 +1,85 @@
+/*
+let nombre = prompt('Como es tu nombre?');
+let edad = prompt('cuantos años tienes?');
+
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años`;
+
+*/
+
+
+/***********************************
+ * Var - let and const
+
+
+//var primerNombre = 'Daniel'; - camelcase
+//var primer_nombre = 'Daniel'; - underscore
+//var PrimerNombre = 'Daniel'; - pascal case
+
+var nombre = 'Daniel';
+console.log(nombre);
+
+
+let nombre = 'Daniel';
+let apellido = 'Garcia';
+console.log(`${nombre} ${apellido}`);
+
+
+const nombre = 'Daniel';
+console.log(nombre);
+
+
+
+let mensaje = 'Aprendiendo JavaScript, HTML, CSS';
+
+console.log(mensaje.indexOf('JavaScript'));
+console.log(mensaje.toUpperCase());
+console.log(mensaje.toLowerCase());
+console.log(mensaje.substring(0,16));
+console.log(mensaje.slice(-7));
+console.log(mensaje.split(', '));
+console.log(mensaje.replace('CSS', 'PHP'));
+console.log(mensaje.includes('CSS'));
+
+let tecnologia = 'JavaScript' + ' ';
+console.log(tecnologia.repeat(5));
+
+*/
+
+
+/**************************************************
+ *Numeros
+ */
+
+/*
+let numero1 = 30;
+let numero2 = 20;
+
+
+let resultado;
+
+//Modulo
+resultado = numero1 % numero2;
+//PI
+resultado = Math.PI;
+//redondear
+resultado = Math.round(2.8);
+//redondear hacia arriba
+resultado = Math.ceil(2.3);
+//redondear hacia abajo
+resultado = Math.floor(2.6);
+//raiz cuadrada
+resultado = Math.sqrt(145);
+//numero absoluto
+resultado = Math.abs(numero2);
+//potencia
+resultado = Math.pow(35, 2);
+//numero minimo
+resultado = Math.min(2,6,9,5);
+//maximo
+resultado = Math.max(2,6,9,5);
+//aleatorio
+resultado = Math.random();
+
+console.log(resultado);
+
+*/
