dcef7dcdd53a8b60ab48b2dcc116f5f4d1d8f8d0
DaGarcia39 <dagarcia282@misena.edu.co>
2020-09-02 10:35:34 -0500

    Actualizacion del curso, con los nuevos cambios

--- app.js
+++ app.js
@@ -83,3 +83,117 @@ resultado = Math.random();
 console.log(resultado);
 
 */
+
+
+/************************************************
+* Video 16 - tipos de datos
+*/
+/*
+
+Datos primitivos:
+- String
+- Number
+- Boolean
+- Null
+- Undefined
+- Symbol
+
+Datos de referencia:
+- Array
+- Objetcs
+- Date
+
+let valor;
+
+valor = 'Daniel';
+valor = 20;
+valor = 20.5;
+valor = -19;
+valor = '20';
+valor = true;
+valor = false;
+valor = undefined;
+valor = null;
+valor = Symbol('Yeah');
+valor = [1,5,9];
+valor = {
+    nombre: 'Alejandro',
+    edad: 20
+};
+
+console.log(typeof valor);
+*/
+
+
+/*******************************************
+ * Operadores de comparacion
+ */
+/*
+const numero1 = 50;
+const numero2 = 20;
+const numero3 = '20';
+
+//Comparadores <>
+//console.log( numero1 < numero2 );
+//console.log( numero1 > numero2 );
+
+//Comparador de igual
+//console.log( numero1 == numero2 );
+//console.log( numero2 == numero3 );
+//console.log( numero2 === numero3 );
+
+//diferentes
+//console.log( numero1 != numero2 );
+//console.log( numero2 != numero3 );
+
+//comparando string
+//console.log( 'Buenas' == ' Buenas' );
+//Mayusculas son de menor valor y las minusculas de mayor
+//console.log( 'A' > 'z' );
+*/
+
+
+/*************************************************
+ * Convertir de String a numero
+
+
+Number()
+parseInt() - parseInt(string, x);
+parseFloat()
+
+toFixed()
+
+
+
+let num1 = 20,
+    num2 = 'Diez',
+    num3 = '10101010',
+    num4 = '20px',
+    num5 = 21.5786765,
+    num6 = true,
+    num7 = -5;
+
+console.log(parseInt(num1));
+console.log(Number(num1));
+console.log(parseFloat(num1));
+
+*/
+
+/***********************************************
+ * Convertir de numero a string
+ */
+
+let dato;
+
+dato = 4;
+
+
+ //String() o toString()
+
+dato = 20;
+dato = true;
+dato = [1,2,3];
+
+dato = dato.toString();
+console.log(dato);
+console.log(dato.length);
---
+++ js/cambio.js
@@ -0,0 +1,6 @@
+function miFunctionPrender() {
+    document.getElementById('imagen1').src = '../img/prendido.jpg';
+}
+function miFunctionApagar() {
+    document.getElementById('imagen1').src = '../img/apagado.jpg';
+}
